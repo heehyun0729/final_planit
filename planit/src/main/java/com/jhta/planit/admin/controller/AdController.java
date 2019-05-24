309fc2d35d9276a2d82756fd4e413dc0de68c7f1
JHTA <JHTA@JHTA-PC>
2019-05-24 18:00:41 +0900

    test

--- planit/src/main/java/com/jhta/planit/admin/controller/AdController.java
+++ planit/src/main/java/com/jhta/planit/admin/controller/AdController.java
@@ -1,12 +1,23 @@
 package com.jhta.planit.admin.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.jhta.planit.admin.service.AdService;
+import com.jhta.planit.admin.vo.AdVo;
 
 @Controller
 public class AdController {
+	@Autowired
+	private AdService service;
 	@RequestMapping(value="/adminHome", method=RequestMethod.GET)
 	public String adminHome() {
 		return ".admin.adminHome";
@@ -20,8 +31,21 @@ public class AdController {
 		return ".admin.adminAdRequestForm";
 	}
 	@RequestMapping(value="/adminAdRequestForm", method=RequestMethod.POST)
-	public String adminAdRequestFormPost(String id, int price, String startDate, String endDate, int chance, String company, MultipartFile file, String url) {
-		
+	public String adminAdRequestFormPost(String id, int price, String startDate, String endDate, @RequestParam(value="chance", defaultValue="0")int chance, String company, MultipartFile file, String url) {
 		return ".admin.adminAdRequestFormOk";
-	}	
+	}
+	@RequestMapping(value="/adminAdGetChance", produces="application/json;charset=utf-8")
+	@ResponseBody
+	public HashMap<String, Object> adminAdGetChance(String startDate, String endDate) {
+		HashMap<String, Object> map=new HashMap<String, Object>();
+		map.put("startDate", startDate);
+		map.put("endDate", endDate);
+		List<AdVo> list=service.checkPeriod(map);
+		int chance=100;
+		for(AdVo vo:list) {
+			chance-=vo.getAd_chance();
+		}
+		map.put("chance", chance);
+		return map;
+	}
 }
