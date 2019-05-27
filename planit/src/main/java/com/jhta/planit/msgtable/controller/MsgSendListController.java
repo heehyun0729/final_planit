52909e47eb8083ee5ccbd29cbefb584d87044e45
JHTA <JHTA@JHTA-PC>
2019-05-27 14:26:12 +0900

    쪽지관련 기본 셋팅 (구현XX)

---
+++ planit/src/main/java/com/jhta/planit/msgtable/controller/MsgSendListController.java
@@ -0,0 +1,51 @@
+package com.jhta.planit.msgtable.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jhta.planit.msgtable.service.MsgTableService;
+import com.jhta.planit.msgtable.vo.MsgTableVo;
+import com.jhta.util.PageUtil;
+
+@Controller
+public class MsgSendListController {
+	@Autowired private MsgTableService service;
+	
+	@RequestMapping("/msgSendList")
+	public ModelAndView list(@RequestParam(value="pageNum",defaultValue = "1")int pageNum,
+			String field,String keyword,String memId,String msgDelete,String msgType) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("field",field);
+		map.put("keyword",keyword);
+		map.put("mem_id","lunar");
+		map.put("MSGDELETE",msgDelete);
+		if(msgType.equals("SEND")) {
+			msgType="sendMemId";
+		}else if(msgType.equals("RECEIVE")) {
+			msgType="receiveMemId";
+		}
+		map.put("type", msgType);
+		
+		int totalRowCount = service.count(map);
+		PageUtil pu = new PageUtil(pageNum,totalRowCount,5,5);
+		map.put("startRow",pu.getStartRow());
+		map.put("endRow",pu.getEndRow());
+		List<MsgTableVo> list = service.list(map);
+		
+		ModelAndView mv = new ModelAndView("member_myMsgList");
+		mv.addObject("list", list);
+		mv.addObject("type", "SEND");
+		mv.addObject("pu", pu);
+		mv.addObject("mem_id", memId);
+		mv.addObject("msgDelete", msgDelete);
+		mv.addObject("field", field);
+		mv.addObject("keyword", keyword);
+		return mv;
+	}
+}
---
+++ planit/src/main/java/com/jhta/planit/msgtable/service/MsgTableService.java
@@ -0,0 +1,45 @@
+package com.jhta.planit.msgtable.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jhta.planit.msgtable.dao.MsgTableDao;
+import com.jhta.planit.msgtable.vo.MsgTableVo;
+
+@Service
+public class MsgTableService {
+	@Autowired private MsgTableDao dao;
+	public void setDao(MsgTableDao dao) {
+		this.dao =dao;
+	}
+	public int insert(MsgTableVo vo) {
+		return dao.insert(vo);
+	}
+	public int count(HashMap<String, Object> map) {
+		return dao.count(map);
+	}
+	public List<MsgTableVo> list(HashMap<String, Object> map){
+		return dao.list(map);
+	}
+	public int delete(int num){
+		return dao.delete(num);
+	}
+	public int update1(MsgTableVo vo){
+		return dao.update1(vo);
+	}
+	public int updateMsgCheck(MsgTableVo vo){
+		return dao.update1(vo);
+	}
+	public MsgTableVo getInfo(int num){
+		return dao.find(num);
+	}
+	public MsgTableVo next(int num) {
+		return dao.next(num);
+	}
+	public MsgTableVo prev(int num) {
+		return dao.prev(num);
+	}
+}
