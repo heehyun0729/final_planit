2defccf97e9f35d4d325d7a26e55f04ad3b01f88
JHTA <JHTA@JHTA-PC>
2019-05-28 15:50:21 +0900

    지도 검색하면 마커, 인포윈도우 추가

--- planit/src/main/webapp/WEB-INF/views/plan/planner.jsp
+++ planit/src/main/webapp/WEB-INF/views/plan/planner.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+	<script async defer type="text/javascript" src="//maps.googleapis.com/maps/api/js?key=AIzaSyBJZmVpy1Zt3vbL5tusNVtcsJQnGjMLOQo&callback=initMap&libraries=places"></script>
+	
 <div id="planner">
 	<div id="leftBox">
 		<h1>플래너</h1>
--- planit/src/main/webapp/resources/js/plan/map.js
+++ planit/src/main/webapp/resources/js/plan/map.js
@@ -1,4 +1,5 @@
-function initAutocomplete() {
+function initMap() {
+	// 지도 초기화
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 46.519, lng: 6.632},
       zoom: 5,
@@ -6,76 +7,72 @@ function initAutocomplete() {
       scrollwheel : true
     });
     
-    // Create the search box and link it to the UI element.
-    var options = {
-    		  types: ['(cities)']
-    		 };
+    // 자동완성검색
+    var options = { types: ['(cities)'] };	// 장소 검색 범위를 도시로 제한
     var input = document.getElementById('address');
     var autocomplete = new google.maps.places.Autocomplete(input, options);
     
     autocomplete.bindTo('bounds', map);
     
     autocomplete.setFields(
-            ['address_components', 'geometry', 'name']);
+            ['address_components', 'geometry', 'name', 'place_id']);
   
     var geocoder = new google.maps.Geocoder();
     
     var infowindow = new google.maps.InfoWindow();
     var marker = new google.maps.Marker();
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
+    
+    var cnt = 0;
+    // 자동완성검색 선택했을 때 실행할 리스너 ==> 마커 찍고 인포윈도우 띄우기
     autocomplete.addListener('place_changed', function() {
-		infowindow.close();
-        marker.setMap(null);
-          
-		var place = autocomplete.getPlace();
+		var place = autocomplete.getPlace();	// 장소(도시) 정보 가져오기
+		// 지리 정보 받아서 마커, 인포윈도우 표시
+      //  geocodeAddress(geocoder, map, place, infowindow, marker);
 		
-		 if (!place.geometry) {
-         // User entered the name of a Place that was not suggested and
-         // pressed the Enter key, or the Place Details request failed.
-         window.alert("No details available for input: '" + place.name + "'");
-         return;
-		 }
-
-		 if (place.geometry.viewport) {
-            map.setCenter(place.geometry.location);
-            map.setZoom(5);  
-          }
-
-        // Create a marker for each place.
-        marker = new google.maps.Marker({
-          map: map,
-          icon: null,
-          title: place.name,
-          position: place.geometry.location
-        });
-        
-        // 여기부터~~~
-        geocodeAddress(geocoder, map);
-        
-		 var country = '';
-         if (place.address_components) {
-        	 country = place.address_components[2] && place.address_components[2].short_name;
-         }
-
-         infowindow = new google.maps.InfoWindow({
-        	 content: place.name + ' ' + country
-         });
-         infowindow.open(map, marker);
-		 
-    });
-  }
-function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({'address': address}, function(results, status) {
-      if (status === 'OK') {
-        resultsMap.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: resultsMap,
-          position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
+		$.getJSON('/planit/googleMap', {
+			placeid: place.place_id,
+			key: 'AIzaSyBJZmVpy1Zt3vbL5tusNVtcsJQnGjMLOQo'
+		}, function(data) {
+			var city = "";
+			var country = "";
+			var address = data.result.address_components;
+			for(var i = 0 ; i < address.length ; i++){
+				if(address[i].types[0] == 'locality'){
+					city = address[i].short_name;
+				}
+				if(address[i].types[0] == 'country'){
+					country = address[i].long_name;
+				}
+			}
+			// 장소 위치(위도, 경도)로 도시 정보 받아오기 ==> 마커, 인포윈도우 표시
+		    geocoder.geocode({'location': place.geometry.location}, function(results, status) {
+		      if (status === 'OK') {
+	    		// 기존 마커, 인포윈도우 삭제
+	    		infowindow.close();
+	    	    marker.setMap(null);
+		        map.setCenter(place.geometry.location);
+		        // 마커 만들기
+		        marker = new google.maps.Marker({
+		          map: map,
+		          title: place.name,
+		          position: place.geometry.location
+		        });
+		        // 마커 위치에 인포윈도우 만들고 도시명, 국가명 띄우기
+		        infowindow = new google.maps.InfoWindow({
+		       	 content: city + ", " + country + " <input type = 'button' id = 'addCity' value = '추가'>"
+		        }, function() {
+			        $("#addCity").on('click', function(){
+		        		console.log(11);
+						$("#leftBox").append("<div>" +
+								city + ", " + country +
+							"</div>");
+					});
+				});
+		        infowindow.open(map, marker);
+		      } else {
+		        alert('해당 도시 정보를 찾을 수 없습니다.');
+		      }
+		    });
+		});
     });
   }
