2b553bce339b0230a09040e7127627624be5dd25
LimSR <orewasolo95@naver.com>
2019-05-28 12:14:42 +0900

    아이디 유효성 체크 검사 완료

--- planit/src/main/webapp/resources/js/members/MemberScript.js
+++ planit/src/main/webapp/resources/js/members/MemberScript.js
@@ -9,18 +9,20 @@ $(document).ready(function() {
 	var chknick=false;
 	var chkemail=false;
 	var RegexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-	var RegexId =  /^[a-z0-9]{5,13}/g;
+	var RegexId =  /^[a-z0-9]{5,13}/;
 	var RegexPwd = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/;
-	$("#mem_id").on("propertychange change keyup paste input", function() {
+	$("#mem_id").on("keyup", function() {
 		chkid=false;
 		$("#idcheckresult").text("");
 		let ckid=$.trim($("#mem_id").val());
 		console.log("아이디 유효성: "+ RegexId.test(ckid));
-		console.log("아이디: "+$("#mem_id").val());
+		console.log("아이디: "+ ckid);
+		console.log("아이디: "+ ckid.length);
 		console.log("숫자포함: "+ /[0-9]+/.test(ckid));
-		console.log("문자포함: "+ ckid.search(/^[a-z]$/));
-		///^[0-9]$/g.test(ckid)&&/^[a-z]$/g.test(ckid)
-		if(RegexId.test(ckid) && (/[0-9]+/.test(ckid) && ckid.search(/^[a-z]$/)>=0)){
+		console.log("문자포함: "+ /[a-z]+/.test(ckid));
+		console.log(RegexId.test(ckid) && (/[0-9]+/.test(ckid) && /[a-z]+/.test(ckid)));
+		
+		if(RegexId.test(ckid) && (/[0-9]+/.test(ckid) && /[a-z]+/.test(ckid))){
 			$("#idcheck").prop("disabled", false);
 		}else{
 			$("#idcheck").prop("disabled", true);
