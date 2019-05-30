ce9ff34f0cee5b280a3d7f01c4f296d3d1139745
LimSR <orewasolo95@naver.com>
2019-05-30 14:40:47 +0900

    이메일 인증 보내기 완료(수신 미완료)
    - spring 버전이 4.1.9에서 4.1.5로 수정되었습니다. 오작동이 있는지 확인 바랍니다.

--- planit/src/main/java/com/jhta/planit/user/controller/MembersJoinController.java
+++ planit/src/main/java/com/jhta/planit/user/controller/MembersJoinController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.jhta.planit.user.service.MembersService;
 import com.jhta.planit.user.vo.MembersVo;
@@ -26,9 +27,12 @@ public class MembersJoinController {
 	}
 
 	@RequestMapping(value = "/nomaljoin", method = RequestMethod.POST)
-	public String join(MembersVo vo, Model model) {
+	public String join(MembersVo vo, Model model,RedirectAttributes attributes) throws Exception{
 		int n = service.membersJoin(vo);
+
+		System.out.println("@@@@@!!!!!!!!! testcontroller !!!!!!!!!!!!@@@@@@@@@@@@");
 		if (n>0) {
+			attributes.addFlashAttribute("authMsg", "메일인증을 완료해 주시기 바랍니다");
 			return "redirect:/"	;
 		}else {
 			model.addAttribute("vo", vo);
@@ -40,7 +44,6 @@ public class MembersJoinController {
 	@ResponseBody
 	public int idcheck(HttpServletRequest request) {
 		String mem_id = request.getParameter("mem_id");
-		System.out.println(mem_id);
 		MembersVo vo = service.idCheck(mem_id);
 
 		int result = 0;
@@ -55,7 +58,6 @@ public class MembersJoinController {
 	@ResponseBody
 	public int nickcheck(HttpServletRequest request) {
 		String mem_nickname = request.getParameter("mem_nickname");
-		System.out.println(mem_nickname);
 		MembersVo vo = service.nickCheck(mem_nickname);
 
 		int result = 0;
@@ -70,7 +72,6 @@ public class MembersJoinController {
 	@ResponseBody
 	public int emailcheck(HttpServletRequest request) {
 		String mem_email = request.getParameter("mem_email");
-		System.out.println(mem_email);
 		MembersVo vo = service.emailCheck(mem_email);
 
 		int result = 0;
--- planit/src/main/java/com/jhta/planit/user/service/MembersService.java
+++ planit/src/main/java/com/jhta/planit/user/service/MembersService.java
@@ -2,22 +2,49 @@ package com.jhta.planit.user.service;
 
 import java.util.HashMap;
 
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jhta.planit.user.dao.MembersDao;
+import com.jhta.planit.user.security.AuthenticationKeyGeneration;
+import com.jhta.planit.user.security.MailHandler;
 import com.jhta.planit.user.vo.MembersVo;
 
 @Service
 public class MembersService {
 	@Autowired private MembersDao dao;
 	@Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
+	@Inject
+    private JavaMailSender mailSender;
 	
-	public int membersJoin(MembersVo vo) {
+	@Transactional
+	public int membersJoin(MembersVo vo) throws Exception {
 		String encodingPwd=bCryptPasswordEncoder.encode(vo.getMem_pwd());
 		vo.setMem_pwd(encodingPwd);
-		return dao.membersJoin(vo);
+		
+		int n=dao.membersJoin(vo);
+		String key = new AuthenticationKeyGeneration().getKey(50, false); // 인증키 생성
+		HashMap<String, Object> map = new HashMap<String, Object>();
+
+		map.put("mem_email", vo.getMem_email());
+		map.put("authKey", key);
+		map.put("mem_id", vo.getMem_id());
+		dao.createAuthKey(map);
+
+		MailHandler sendMail = new MailHandler(mailSender);
+		sendMail.setSubject("[Planit 서비스 이메일 인증]");
+		sendMail.setText(
+				new StringBuffer().append("<h1>메일인증</h1>").append("<a href='http://localhost:9090/planit/member/emailConfirm?user_email=").append(vo.getMem_email()).append("&key=").append(key).append("' target='_blenk'>이메일 인증 확인</a>").toString());
+		sendMail.setFrom("limsr95@gmail.com", "Planit");
+		sendMail.setTo(vo.getMem_email());
+		sendMail.send();
+		System.out.println("@@@@@!!!!!!!!! testservice !!!!!!!!!!!!@@@@@@@@@@@@");
+		return n;
 	}
 
 	public boolean login(HashMap<String, String> map) {
