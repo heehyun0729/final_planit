01f78f838f387cc99d26293bc24397b6fc9a2a9e
heehyun0729 <hhkim0729@gmail.com>
2019-06-08 15:56:21 +0900

    예약 파일 이름 수정

--- planit/src/main/java/com/jhta/planit/reservation/controller/ReservationController.java
+++ planit/src/main/java/com/jhta/planit/reservation/controller/ReservationController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ReservationFormController {
-	@RequestMapping(value="/reservationHome", method=RequestMethod.GET)
-	public String adminHome() {
-		return ".reservation.reservationHome";
+public class ReservationController {
+	@RequestMapping(value="/reservation", method=RequestMethod.GET)
+	public String list() {
+		return ".reservation.rsvnList";
 	}
 }
