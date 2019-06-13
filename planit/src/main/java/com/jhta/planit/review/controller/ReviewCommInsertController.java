7e0dc9610d0b3b6f8bea60f1797053af7d360fd9
USER <USER@DESKTOP-Q0F7VPT>
2019-06-13 17:48:02 +0900

    review insert delete image update like detail controller 대량 수정 및 추가

--- planit/src/main/java/com/jhta/planit/review/controller/ReviewCommInsertController.java
+++ planit/src/main/java/com/jhta/planit/review/controller/ReviewCommInsertController.java
@@ -1,36 +1,69 @@
 package com.jhta.planit.review.controller;
 
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jhta.planit.review.service.ReviewCommService;
 import com.jhta.planit.review.vo.ReviewCommVo;
+import com.jhta.util.PageUtil;
 
 @Controller
 public class ReviewCommInsertController {
 	@Autowired private ReviewCommService service;
 	
 	//댓글등록
-	@RequestMapping (value="/review/reviewComm.do",produces= "application/xml;charset=utf-8")
-	@ResponseBody
-	public String insert(ReviewCommVo vo,int review_num,String mem_id,HttpSession session) {
-		
-		service.insert(vo);
-		
-		//xml답
-		StringBuffer sb=new StringBuffer();
-		sb.append("<?xml version='1.0' encoding='utf-8'?>"); 
-		sb.append("<rcomm_num>" + vo.getRcomm_num() + "</rcomm_num>");
-		System.out.println("xml:" + sb.toString());
-		return sb.toString();
-	}
-	
-	//@RequestMapping(value="", method=RequestMethod.POST)
-	
-}
+			@RequestMapping (value="/reviewComm/insert",produces= "application/xml;charset=utf-8")
+			@ResponseBody
+			public String insert(ReviewCommVo vo,HttpSession session) {
+				
+				service.insert(vo);
+				
+				//xml답
+				StringBuffer sb=new StringBuffer();
+				sb.append("<?xml version='1.0' encoding='utf-8'?>"); 
+				sb.append("<result>success</result>");
+				System.out.println("xml:" + sb.toString());
+				return sb.toString();
+			}
+			
+			@RequestMapping(value="/reviewCommVo/getList",produces="application/xml;charset=utf-8")
+			@ResponseBody
+			public String getList(@RequestParam(value="pageNum",defaultValue="1")int pageNum,int review_num) {
+				//댓글에 대한 댓글수
+				int count=service.getCount(review_num);
+				PageUtil pu=new PageUtil(pageNum, count,5,5);
+				HashMap<String,Integer> map=new HashMap<String, Integer>();
+				map.put("startRow", pu.getStartRow());
+				map.put("endRow", pu.getEndRow());
+				map.put("review_num", review_num);
+				
+				List<ReviewCommVo> list=service.list(map);
+				StringBuffer sb=new StringBuffer();
+				sb.append("<?xml version='1.0' encoding='utf-8' ?>");
+				sb.append("<result>");
+				for(ReviewCommVo vo:list) {
+					sb.append("<list>");
+					sb.append("<review_num>" + vo.getReview_num()+ "</review_num>");
+					sb.append("<mem_id>" + vo.getMem_id()+ "</mem_id>");
+					sb.append("<rcomm_num>" + vo.getRcomm_num() + "</rcomm_num>");
+					sb.append("<rcomm_content>" + vo.getRcomm_content()+ "</rcomm_content>");
+					sb.append("<rcomm_date>" + vo.getRcomm_date()+ "</rcomm_date>");
+					sb.append("</list>");
+				}
+				sb.append("<startPageNum>" + pu.getStartPageNum() + "</startPageNum>");//시작페이지1
+				sb.append("<endPageNum>" + pu.getEndPageNum()+ "</endPageNum>");//끝페이지5
+				sb.append("<pageCount>" + pu.getTotalPageCount()+ "</pageCount>");//페이지의 갯수
+				sb.append("</result>");
+				System.out.println("xml:" + sb.toString());
+				return sb.toString();
+				
+			}
+		}
--- planit/src/main/java/com/jhta/planit/review/controller/ReviewDeleteController.java
+++ planit/src/main/java/com/jhta/planit/review/controller/ReviewDeleteController.java
@@ -1,32 +1,27 @@
 package com.jhta.planit.review.controller;
-//package com.jhta.planit.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//
-//
-//
-//import com.jhta.planit.review.service.ReviewService;
-//
-//@Controller
-//public class ReviewDeleteController {
-//	@Autowired private ReviewService service;
-//	
-//	@RequestMapping(value="/review/reviewDetail")
-//	public String delete(int review_num, int reviewImge_num ,Model model) {
-//		int n=service.delete(review_num);
-//		if(n>0) {
-//			//리다이렉트방식으로 페이지 이동하기(컨트롤러호출)
-//			return "redirect:/review/reviewDetail.do";
-//		}else {
-//			model.addAttribute("result","fail");
-//			return "result";
-//		}
-//	}
-//	
-//	@RequestMapping(value="/review/reviewDetail")
-//	
-//}
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jhta.planit.review.service.ReviewService;
+
+@Controller
+public class ReviewDeleteController {
+	@Autowired private ReviewService service;
+	
+	@RequestMapping(value="/reviewDelete")
+	public String delete(@RequestParam(value="review_num", required=true)int review_num ,Model model) {
+		System.out.println("11:"+review_num);
+		int n=service.updateReviewChk(review_num);
+		if(n>0) {
+			//리다이렉트방식으로 페이지 이동하기(컨트롤러호출)
+			return "redirect:/review/reviewList";
+		}else {
+			model.addAttribute("result","fail");
+			return "result";
+		}
+	}
+}
