9eec5090d5a05b20a3c4ee2a7f209df9abc55c97
USER <USER@DESKTOP-Q0F7VPT>
2019-06-13 17:31:31 +0900

    reviewvo, mapper(추가 수정 삭제 좋아요등) 파일 대량 수정

--- planit/src/main/java/com/jhta/planit/review/vo/ReviewImageVo.java
+++ planit/src/main/java/com/jhta/planit/review/vo/ReviewImageVo.java
@@ -5,48 +5,50 @@ public class ReviewImageVo {
 	private int review_num;
 	private String reviewImge_orgImg;
 	private String reviewImge_saveImg;
-	//REVIEWIMAGE_SAVEIMG
+	private int reviewImageChk;
 	public ReviewImageVo() {}
-
-	public ReviewImageVo(int reviewImge_num, int review_num, String reviewImge_orgImg, String reviewImge_saveImg) {
+	
+	public ReviewImageVo(int reviewImge_num, int review_num, String reviewImge_orgImg, String reviewImge_saveImg,
+			int reviewImageChk) {
 		super();
 		this.reviewImge_num = reviewImge_num;
 		this.review_num = review_num;
 		this.reviewImge_orgImg = reviewImge_orgImg;
 		this.reviewImge_saveImg = reviewImge_saveImg;
+		this.reviewImageChk = reviewImageChk;
 	}
-
 	public int getReviewImge_num() {
 		return reviewImge_num;
 	}
-
 	public void setReviewImge_num(int reviewImge_num) {
 		this.reviewImge_num = reviewImge_num;
 	}
-
 	public int getReview_num() {
 		return review_num;
 	}
-
 	public void setReview_num(int review_num) {
 		this.review_num = review_num;
 	}
-
 	public String getReviewImge_orgImg() {
 		return reviewImge_orgImg;
 	}
-
 	public void setReviewImge_orgImg(String reviewImge_orgImg) {
 		this.reviewImge_orgImg = reviewImge_orgImg;
 	}
-
 	public String getReviewImge_saveImg() {
 		return reviewImge_saveImg;
 	}
-
 	public void setReviewImge_saveImg(String reviewImge_saveImg) {
 		this.reviewImge_saveImg = reviewImge_saveImg;
 	}
+	public int getReviewImageChk() {
+		return reviewImageChk;
+	}
+	public void setReviewImageChk(int reviewImageChk) {
+		this.reviewImageChk = reviewImageChk;
+	}
+
+	
 
 	
 }
