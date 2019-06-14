18c8681dce13a2ca8b8717b46e7c66f5dc0975b8
heehyun0729 <hhkim0729@gmail.com>
2019-06-14 11:34:38 +0900

    판매자 예약 목록 페이징 완성 / 예약자에게 쪽지보내기 추가

--- planit/src/main/webapp/WEB-INF/views/layout.jsp
+++ planit/src/main/webapp/WEB-INF/views/layout.jsp
@@ -39,6 +39,7 @@
 <script type="text/javascript" src = "<c:url value = '/resources/js/qna/insert.js'/>"></script>
 <script type="text/javascript" src = "<c:url value = '/resources/js/qna/update.js'/>"></script>
 <script type="text/javascript" src = "<c:url value = '/resources/js/members/myRsvn.js'/>"></script>
+<script type="text/javascript" src = "<c:url value = '/resources/js/seller/sellRsvn.js'/>"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.css" rel="stylesheet">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-bs4.js"></script>
 </head>
--- planit/src/main/webapp/resources/js/reservation/list.js
+++ planit/src/main/webapp/resources/js/reservation/list.js
@@ -23,29 +23,6 @@ $(function() {
 		dateFormat: "yy-mm-dd",
 		numberOfMonths: 2
     });
-   
-   var from1 = $( "#sellCheckin" ).datepicker({
-   	dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"],
-		monthNames: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", 
-                       "8월", "9월", "10월", "11월", "12월"],	
-		yearSuffix: "년",	
-		showMonthAfterYear: true,	
-		dateFormat: "yy-mm-dd",
-       numberOfMonths: 2,
-		onSelect: function(date) {
-			var d = new Date(date);
-			$("#sellCheckout").datepicker("option", "minDate", new Date(d.getFullYear(),d.getMonth(), d.getDate() + eval(1)));
-		}
-     }) ;
-  var to1 = $( "#sellCheckout" ).datepicker({
-   	dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"],
-		monthNames: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", 
-                       "8월", "9월", "10월", "11월", "12월"],	
-		yearSuffix: "년",	
-		showMonthAfterYear: true,	
-		dateFormat: "yy-mm-dd",
-		numberOfMonths: 2
-   });
 });
 
 function increaseCnt() {
@@ -61,8 +38,4 @@ function decreaseCnt() {
 	cnt < 2 ? cnt = 2 : cnt;
 	cnt--;
 	$("#rsvnCnt").val(cnt);
-}
-
-function sellRsvnSubmit() {
-	$("#sellRsvnForm").submit();
 }
